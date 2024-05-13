00329327693b679df1afa2459d1aedd2c4ec3af3
ee <sample@mail.com>
2024-05-13 22:33:58 +0800

    asdf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fontanilla, Jule Ethan E.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/app.css" type="text/css">
+    <link rel="stylesheet" href="./css/app.css" type="text/css">
 </head>
 
 <body>
@@ -30,7 +30,7 @@
                     </div>
                 </div>
                 <div class="col">
-                    <img class="rounded-circle justify-content-end img" src="./image.jpeg" style="width: 500px; border: white 5px solid">
+                    <img class="rounded-circle justify-content-end img" src="{{asset('./image.jpeg')}}" style="width: 500px; border: white 5px solid">
                 </div>
             </div>
         </div>
@@ -53,7 +53,7 @@
                     <div class="col">
                         <h1 class="fw-bold text-center">This my cat</h1>
                         <div class="text-center">
-                            <img class="rounded-circle justify-content-end" src="./cat.jpg" style="width: 300px; border: white 5px solid">
+                            <img class="rounded-circle justify-content-end" src="{{asset('./cat.jpg')}}" style="width: 300px; border: white 5px solid">
                         </div>
                     </div>
                 </div>
@@ -81,7 +81,7 @@
             <div class="row align-items-center">
                 <div class="col">
                     <div class="text-center">
-                        <img class=" justify-content-end" src="./user.svg" style="width: 300px; ">
+                        <img class=" justify-content-end" src="{{asset('./user.svg')}}" style="width: 300px; ">
                     </div>
                 </div>
                 <div class="col">
